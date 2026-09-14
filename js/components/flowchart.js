308535ba7da73f5387b706ed2500c8cb8d088020
birk <bjarkelv@gmail.com>
2026-09-14 10:00:42 +0200

    more flowchart js
    made more flowchart js, making the active toggles work and started on link insertion

--- js/components/flowchart.js
+++ js/components/flowchart.js
@@ -1,20 +1,100 @@
 
+const siteCheck = document.querySelector(".homepage-main")
 
+import { fetchInfoboxData } from "./infobox.js"
+const data = await fetchInfoboxData();
 
 export function flowchart(){
-    const flowchartBtn1 = document.querySelectorAll(".flowchart-btn1")
-    const flowchartBtn2 = document.querySelectorAll(".flowchart-btn2")
-    const flowchartBtn3 = document.querySelectorAll(".flowchart-btn3")
-    const flowchartBtn4 = document.querySelectorAll(".flowchart-btn4")
-    const flowchartMenu1 = document.querySelector(".flowchart-menu1")
-    const flowchartMenu2 = document.querySelector(".flowchart-menu2")
-    const flowchartMenu3 = document.querySelector(".flowchart-menu3")
-    const flowchartMenu4 = document.querySelector(".flowchart-menu4")
 
-    flowchartBtn1.forEach((btn, index) => {
-        btn.addEventListener("click", () => {
-            flowchartMenu1.innerHTML = index
-            flowchartMenu1.classList.add("active")
+    if (siteCheck){
+
+        //#region CONSTS
+
+        const flowchartBtns = document.querySelectorAll(".flowchart-btn")
+        const flowchartMenus = document.querySelectorAll(".flowchart-menu")
+    
+        const flowchartBtn1 = document.querySelectorAll(".flowchart-btn1")
+        const flowchartBtn2 = document.querySelectorAll(".flowchart-btn2")
+        const flowchartBtn3 = document.querySelectorAll(".flowchart-btn3")
+        const flowchartBtn4 = document.querySelectorAll(".flowchart-btn4")
+    
+        const flowchartMenu1 = document.querySelector(".flowchart-menu1")
+        const flowchartMenu2 = document.querySelector(".flowchart-menu2")
+        const flowchartMenu3 = document.querySelector(".flowchart-menu3")
+        const flowchartMenu4 = document.querySelector(".flowchart-menu4")
+    
+        //#endregion CONSTS END
+    
+        //#region GENERAL TOGGLES
+    
+        function removeActiveElements(){
+            flowchartBtns.forEach((btn) => {
+                btn.classList.remove("active")
+            })
+            flowchartMenus.forEach((menu) => {
+                menu.classList.remove("active")
+            })
+        }
+        
+    
+        flowchartBtns.forEach((btn, index) => {
+            btn.addEventListener("click", () => {
+                removeActiveElements()
+                flowchartBtns[index].classList.add("active")
+            })
         })
-    })
+
+        //#endregion GENERAL TOGGLES END
+    
+        //#region SPECIFIC TOGGLES
+
+        flowchartBtn1.forEach((btn, index) => {
+            btn.addEventListener("click", () => {
+                flowchartMenu1.classList.add("active")
+                flowchartMenu1.innerHTML = data.map((element) => {
+                        return`<a href="${element.href}?id=${element.id}">${element.title}</a>`
+                    }).join("")
+                /* NEED TO MAKE IT ONLY INPUT ELEMENTS WITH A "category" VALUE OF "character" */
+            })
+        })
+    
+        flowchartBtn2.forEach((btn, index) => {
+            btn.addEventListener("click", () => {
+                flowchartMenu2.classList.add("active")
+                flowchartMenu2.innerHTML = data.map((element) => {
+                        return`<a href="${element.href}?id=${element.id}">${element.title}</a>`
+                    }).join("")
+                /* NEED TO MAKE IT ONLY INPUT ELEMENTS WITH A "category" VALUE OF "battle" */
+            })
+        })
+    
+        flowchartBtn3.forEach((btn, index) => {
+            btn.addEventListener("click", () => {
+                flowchartMenu3.classList.add("active")
+                flowchartMenu3.innerHTML = data.map((element) => {
+                        return`<a href="${element.href}?id=${element.id}">${element.title}</a>`
+                    }).join("")
+                /* NEED TO MAKE IT ONLY INPUT ELEMENTS WITH A "category" VALUE OF "other" */
+            })
+        })
+    
+        flowchartBtn4.forEach((btn, index) => {
+            btn.addEventListener("click", () => {
+                flowchartMenu4.classList.add("active")
+                flowchartMenu4.innerHTML = data.map((element) => {
+                        return`<a href="${element.href}?id=${element.id}">${element.title}</a>`
+                    }).join("")
+                /* NEED TO MAKE IT ONLY INPUT ELEMENTS WITH A "category" VALUE OF "world" */
+            })
+        })
+        
+        //#endregion SPECIFIC TOGGLES END
+
+    }
+
+
+    
+
 }
+
+
