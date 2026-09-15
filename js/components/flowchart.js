3c1d83dc80aa4337c12225a0b8a3524619755bd9
barkmand <bjarkelv@gmail.com>
2026-09-15 18:17:13 +0200

    Finished flowchart js and updated header js
    Finished the flowchart js to make it put in new links every time a new element/page is created in json. Also made categories in the burger menu and made them update in the same way as the flowchart.

--- js/components/flowchart.js
+++ js/components/flowchart.js
@@ -1,100 +1,159 @@
+const siteCheck = document.querySelector(".homepage-main");
 
-const siteCheck = document.querySelector(".homepage-main")
-
-import { fetchInfoboxData } from "./infobox.js"
+import { fetchInfoboxData } from "./infobox.js";
 const data = await fetchInfoboxData();
 
-export function flowchart(){
+export function flowchart() {
+  if (siteCheck) {
+    //#region CONSTS
 
-    if (siteCheck){
+    const flowchartBtns = document.querySelectorAll(".flowchart-btn");
+    const flowchartMenus = document.querySelectorAll(".flowchart-menu");
 
-        //#region CONSTS
+    const flowchartBtn1 = document.querySelectorAll(".flowchart-btn1");
+    const flowchartBtn2 = document.querySelectorAll(".flowchart-btn2");
+    const flowchartBtn3 = document.querySelectorAll(".flowchart-btn3");
+    const flowchartBtn4 = document.querySelectorAll(".flowchart-btn4");
 
-        const flowchartBtns = document.querySelectorAll(".flowchart-btn")
-        const flowchartMenus = document.querySelectorAll(".flowchart-menu")
-    
-        const flowchartBtn1 = document.querySelectorAll(".flowchart-btn1")
-        const flowchartBtn2 = document.querySelectorAll(".flowchart-btn2")
-        const flowchartBtn3 = document.querySelectorAll(".flowchart-btn3")
-        const flowchartBtn4 = document.querySelectorAll(".flowchart-btn4")
-    
-        const flowchartMenu1 = document.querySelector(".flowchart-menu1")
-        const flowchartMenu2 = document.querySelector(".flowchart-menu2")
-        const flowchartMenu3 = document.querySelector(".flowchart-menu3")
-        const flowchartMenu4 = document.querySelector(".flowchart-menu4")
-    
-        //#endregion CONSTS END
-    
-        //#region GENERAL TOGGLES
-    
-        function removeActiveElements(){
-            flowchartBtns.forEach((btn) => {
-                btn.classList.remove("active")
-            })
-            flowchartMenus.forEach((menu) => {
-                menu.classList.remove("active")
-            })
-        }
-        
-    
-        flowchartBtns.forEach((btn, index) => {
-            btn.addEventListener("click", () => {
-                removeActiveElements()
-                flowchartBtns[index].classList.add("active")
-            })
-        })
+    const flowchartMenu1 = document.querySelector(".flowchart-menu1");
+    const flowchartMenu2 = document.querySelector(".flowchart-menu2");
+    const flowchartMenu3 = document.querySelector(".flowchart-menu3");
+    const flowchartMenu4 = document.querySelector(".flowchart-menu4");
 
-        //#endregion GENERAL TOGGLES END
-    
-        //#region SPECIFIC TOGGLES
+    //#endregion CONSTS END
 
-        flowchartBtn1.forEach((btn, index) => {
-            btn.addEventListener("click", () => {
-                flowchartMenu1.classList.add("active")
-                flowchartMenu1.innerHTML = data.map((element) => {
-                        return`<a href="${element.href}?id=${element.id}">${element.title}</a>`
-                    }).join("")
-                /* NEED TO MAKE IT ONLY INPUT ELEMENTS WITH A "category" VALUE OF "character" AND A "subcategory" VALUE OF "the specific button you just pressed" */
-            })
-        })
-    
-        flowchartBtn2.forEach((btn, index) => {
-            btn.addEventListener("click", () => {
-                flowchartMenu2.classList.add("active")
-                flowchartMenu2.innerHTML = data.map((element) => {
-                        return`<a href="${element.href}?id=${element.id}">${element.title}</a>`
-                    }).join("")
-                /* NEED TO MAKE IT ONLY INPUT ELEMENTS WITH A "category" VALUE OF "battle" AND A "subcategory" VALUE OF "the specific button you just pressed" */
-            })
-        })
-    
-        flowchartBtn3.forEach((btn, index) => {
-            btn.addEventListener("click", () => {
-                flowchartMenu3.classList.add("active")
-                flowchartMenu3.innerHTML = data.map((element) => {
-                        return`<a href="${element.href}?id=${element.id}">${element.title}</a>`
-                    }).join("")
-                /* NEED TO MAKE IT ONLY INPUT ELEMENTS WITH A "category" VALUE OF "other" AND A "subcategory" VALUE OF "the specific button you just pressed" */
-            })
-        })
-    
-        flowchartBtn4.forEach((btn, index) => {
-            btn.addEventListener("click", () => {
-                flowchartMenu4.classList.add("active")
-                flowchartMenu4.innerHTML = data.map((element) => {
-                        return`<a href="${element.href}?id=${element.id}">${element.title}</a>`
-                    }).join("")
-                /* NEED TO MAKE IT ONLY INPUT ELEMENTS WITH A "category" VALUE OF "world" AND A "subcategory" VALUE OF "the specific button you just pressed" */
-            })
-        })
-        
-        //#endregion SPECIFIC TOGGLES END
+    //#region GENERAL TOGGLES
 
+    function removeActiveElements() {
+      flowchartBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      flowchartMenus.forEach((menu) => {
+        menu.classList.remove("active");
+      });
     }
 
+    flowchartBtns.forEach((btn, index) => {
+      btn.addEventListener("click", () => {
+        removeActiveElements();
+        flowchartBtns[index].classList.add("active");
+      });
+    });
 
-    
+    //#endregion GENERAL TOGGLES END
 
+    //#region SPECIFIC TOGGLES
+
+    flowchartBtn1.forEach((btn, index) => {
+      btn.addEventListener("click", () => {
+        flowchartMenu1.classList.add("active");
+
+        let categoryCheckResult;
+
+        if (btn.getAttribute("data-set") == "good") {
+          categoryCheckResult = data.filter(
+            (element) =>
+              element.category == "character" && element.subcategory == "good",
+          );
+        }
+        if (btn.getAttribute("data-set") == "bad") {
+          categoryCheckResult = data.filter(
+            (element) =>
+              element.category == "character" && element.subcategory == "bad",
+          );
+        }
+        if (btn.getAttribute("data-set") == "tool") {
+          categoryCheckResult = data.filter((element) => element.category == "character" && element.subcategory == "tool")
+        }
+
+        flowchartMenu1.innerHTML = categoryCheckResult.map((element) => {
+            return `<a href="${element.href}?id=${element.id}">${element.title}</a>`;
+          })
+          .join("");
+      });
+    });
+
+    flowchartBtn2.forEach((btn, index) => {
+      btn.addEventListener("click", () => {
+        flowchartMenu2.classList.add("active");
+
+        let categoryCheckResult;
+
+        if (btn.getAttribute("data-set") == "season1") {
+          categoryCheckResult = data.filter((element) => element.category == "battle" && element.subcategory == "season1",
+          );
+        }
+        if (btn.getAttribute("data-set") == "season2") {
+          categoryCheckResult = data.filter((element) => element.category == "battle" && element.subcategory == "season2",
+          );
+        }
+        if (btn.getAttribute("data-set") == "season3") {
+          categoryCheckResult = data.filter((element) => element.category == "battle" && element.subcategory == "season3")
+        }
+        if (btn.getAttribute("data-set") == "season4") {
+          categoryCheckResult = data.filter((element) => element.category == "battle" && element.subcategory == "season4")
+        }
+        if (btn.getAttribute("data-set") == "season5") {
+          categoryCheckResult = data.filter((element) => element.category == "battle" && element.subcategory == "season5")
+        }
+
+        flowchartMenu2.innerHTML = categoryCheckResult.map((element) => {
+            return `<a href="${element.href}?id=${element.id}">${element.title}</a>`;
+          })
+          .join("");
+      });
+    });
+
+    flowchartBtn3.forEach((btn, index) => {
+      btn.addEventListener("click", () => {
+        flowchartMenu3.classList.add("active");
+
+        let categoryCheckResult;
+
+        if (btn.getAttribute("data-set") == "pre") {
+          categoryCheckResult = data.filter(
+            (element) =>
+              element.category == "event" && element.subcategory == "pre",
+          );
+        }
+        if (btn.getAttribute("data-set") == "mid") {
+          categoryCheckResult = data.filter(
+            (element) =>
+              element.category == "event" && element.subcategory == "mid",
+          );
+        }
+        if (btn.getAttribute("data-set") == "post") {
+          categoryCheckResult = data.filter((element) => element.category == "event" && element.subcategory == "post")
+        }
+
+        flowchartMenu3.innerHTML = categoryCheckResult.map((element) => {
+            return `<a href="${element.href}?id=${element.id}">${element.title}</a>`;
+          })
+          .join("");
+      });
+    });
+
+    flowchartBtn4.forEach((btn, index) => {
+      btn.addEventListener("click", () => {
+        flowchartMenu4.classList.add("active");
+
+        let categoryCheckResult;
+
+        if (btn.getAttribute("data-set") == "terminology") {
+          categoryCheckResult = data.filter(
+            (element) =>
+              element.category == "world" && element.subcategory == "terminology",
+          );
+        }
+        
+
+        flowchartMenu4.innerHTML = categoryCheckResult.map((element) => {
+            return `<a href="${element.href}?id=${element.id}">${element.title}</a>`;
+          })
+          .join("");
+      });
+    });
+
+    //#endregion SPECIFIC TOGGLES END
+  }
 }
-
-
--- js/components/header.js
+++ js/components/header.js
@@ -5,51 +5,86 @@ const data = await fetchInfoboxData();
 
 export function header() {
   if (siteCheck) {
+    /* ${data
+    .map((element) => {
+      return `<a href="${element.href}?id=${element.id}">${element.title}</a>`;
+      })
+      .join("")}
+      <!-- NEED TO MAKE IT ONLY INPUT ELEMENTS WITH A "category" VALUE OF "character"--> */
+
     siteCheck.insertAdjacentHTML(
       "beforeend",
       `
-            
-            <nav class="page-nav">
+        <nav class="page-nav">
             <a href="index.html"><img src="assets/images/Jujutsu_Kaisen_logo_in_Japan.png" class="nav-icon"></a>
             <div class="burger-button">
-            <div class="bar"></div>
-            <div class="bar"></div>
                 <div class="bar"></div>
-                </div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+            </div>
             <div class="burger-menu">
-            <section class="menu-section"><a href="index.html">Frontpage</a></section>
-            <hr class="menu-seperator">
                 <section class="menu-section">
-                <p class="menu-section-btn">Characters</p>
-                <div class="menu-section-container">
-                    ${data
-                      .map((element) => {
-                        return `<a href="${element.href}?id=${element.id}">${element.title}</a>`;
-                      })
-                      .join("")}
-                      <!-- NEED TO MAKE IT ONLY INPUT ELEMENTS WITH A "category" VALUE OF "character"-->
-                </div>
+                    <a href="index.html">Frontpage</a>
                 </section>
                 <hr class="menu-seperator">
                 <section class="menu-section">
-                <p class="menu-section-btn">Battles</p>
-                <div class="menu-section-container">
-                    ${data
-                      .map((element) => {
-                        return `<a href="${element.href}?id=${element.id}">${element.title}</a>`;
-                      })
-                      .join("")}
-                      <!-- NEED TO MAKE IT ONLY INPUT ELEMENTS WITH A "category" VALUE OF "battle"-->
-                </div>
+                    <p class="menu-section-btn">Characters</p>
+                    <div class="menu-section-container" data-set="character"></div>
                 </section>
-                
-                </div>
-                </nav>
-                <section class="page-hero">
-                <h1>Jujutsu Kaisen FanWiki</h1>
+                <hr class="menu-seperator">
+                <section class="menu-section">
+                    <p class="menu-section-btn">Battles</p>
+                    <div class="menu-section-container" data-set="battle"></div>
                 </section>
-                
-                `,
+                <hr class="menu-seperator">
+                <section class="menu-section">
+                    <p class="menu-section-btn">Other Events</p>
+                    <div class="menu-section-container" data-set="event"></div>
+                </section>
+                <hr class="menu-seperator">
+                <section class="menu-section">
+                    <p class="menu-section-btn">World</p>
+                    <div class="menu-section-container" data-set="world"></div>
+                </section>
+            </div>
+        </nav>
+        <section class="page-hero">
+        <h1>Jujutsu Kaisen FanWiki</h1>
+        </section>
+        
+        `,
     );
   }
+  const linkContainers = document.querySelectorAll(".menu-section-container");
+  
+  linkContainers.forEach((container) => {
+    let categoryCheckResult;
+  
+    if (container.getAttribute("data-set") == "character") {
+      categoryCheckResult = data.filter(
+        (element) => element.category == "character",
+      );
+    }
+    if (container.getAttribute("data-set") == "battle") {
+      categoryCheckResult = data.filter(
+        (element) => element.category == "battle",
+      );
+    }
+    if (container.getAttribute("data-set") == "event") {
+      categoryCheckResult = data.filter((element) => element.category == "event");
+    }
+    if (container.getAttribute("data-set") == "world") {
+      categoryCheckResult = data.filter((element) => element.category == "world");
+      console.log(categoryCheckResult);
+    }
+  
+    container.innerHTML = categoryCheckResult
+      .map((element) => {
+        return `<a href="${element.href}?id=${element.id}">${element.title}</a>`;
+      })
+      .join("");
+  
+      console.log(categoryCheckResult);
+    });
+    
 }
